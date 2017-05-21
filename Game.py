efa92a293996d377bd17a5adb7396d897b992f97
40Bit <ayugithub@amitk.me>
2017-05-21 12:06:53 -0700

    Made some changes

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import sys
 
 state = {
+    "hearts": 5,
     "speeds": {
         "speed": 0,
         "xspeed": 0,
@@ -11,10 +12,12 @@ state = {
         "rocks": 0,
         "x-speed hammmer": 0,
         "spiked boomerang": 0,
+        "crystal": 0
         },
+        
     }
 
-            
+
 # different methods
 
 def query_yes_no(question, default="yes"):
@@ -52,16 +55,16 @@ def query_yes_no(question, default="yes"):
 
 def addspeed(type, add_speed):
     global state
-    
+
     state["speeds"][type] += add_speed
     print("have %s %s" % (type, state["speeds"][type]))
 
 
 
-            
+
 def grasslandfile1():
     global state
-    
+
     print("harvested 25 pieces of grass")
     answer11 = query_yes_no("craft grass-knitted pants?? ")
 
@@ -79,10 +82,10 @@ def grasslandfile1():
 
 def eaglemountains():
     global state
-    
+
     for times in range(0, 3):
 
-        
+
         answer16 = query_yes_no("take some rocks??")
 
         if answer16:
@@ -90,9 +93,32 @@ def eaglemountains():
                 state["inventory"]["rocks"] += 3
                 print("this is what you have in inventory")
                 print(state["inventory"])
+                answer9 = query_yes_no("you see something shiny.do you take a closer look??")
                 
+                if answer9:
+                    print("you take a closer look.you see some crystals")
 
-        else:   
+                else:
+                    print("you try to back away from the crystal but you can't.you go look at the shiny thing.it is some crystals.")
+                          
+                answer9 = query_yes_no("do you take the crystal??")
+
+                if answer9:
+                          state["inventory"]["crystal"] += 2
+                          print("this is your inventory:")
+                          print(state["inventory"])
+                          print("you then fall into a hole somehow.then you realize that the crystal was a trap.you were not supposed to take the crystal.you also got hurt from the fall")
+                          print("depleted 1 heart")
+                          state["hearts"] -= 1
+                          print("this is your health:")
+                          print(state["hearts"])
+
+                else:
+                    print("h)ello")
+                
+                          
+
+        else:
             print("this is what you have in inventory")
             print(state["inventory"])
             answer9 = query_yes_no("harvest grass from valley??")
@@ -100,11 +126,13 @@ def eaglemountains():
             if answer9:
                 print("you_________enemy")
                 answer9 = query_yes_no("kill enemy??")
-                
+
                 if answer9:
-                    print("you______//__<- enemy")
+                    print("you______//__<-_enemy")
                     print("harvested 10 grass pieces")
                     state["inventory"]["grass"] += 10
+                    print("your inventory:")
+                    print(state["inventory"])
 
                 else:
                     print("you run up to the the higher cliffs")
@@ -116,14 +144,29 @@ def eaglemountains():
 
                     if answer9:
                         state["inventory"]["spiked boomerang"] += 1
+                        addspeed("speed", 2)
 
+                    else:
+                        print("you__________enemy")
+                        answer9 = query_yes_no("you see another enemy.do you kill it??")
 
+                        if answer9:
+                            print("you___//_<-_enemy")
+
+                        else:
+                            print("enemy depleted 0.5 hearts")
+                            state["hearts"] -= 0.5
+                            print("hearts:")
+                            print(state["hearts"])
+                            
+
+                            
 def n_y_hit_dummy(question):
     global state
-    
+
 
     answer = query_yes_no(question)
- 
+
     if answer:
         print("you      //  <- dummy")
         answer6 = query_yes_no("go to official training grounds??")
@@ -134,16 +177,16 @@ def n_y_hit_dummy(question):
             if answer9:
                 print("earned 3 speed levels")
                 addspeed("speed", 3)
-                
+
                 print("loading grassland")
                 print("grassland loaded")
                 answer7 = query_yes_no("harvest some grass?? ")
-                
+
 
                 if answer7:
                     grasslandfile1()
-                    
-                    
+
+
                 else:
                     print("you____________enemy")
                     answer8 = query_yes_no("kill enemy?? ")
@@ -158,12 +201,12 @@ def n_y_hit_dummy(question):
                 print("loading grassland")
                 print("grassland loaded")
                 answer7 = query_yes_no("harvest some grass?? ")
-                
+
 
                 if answer7:
                     grasslandfile1()
-                    
-                    
+
+
                 else:
                     print("you____________enemy")
                     answer8 = query_yes_no("kill enemy?? ")
@@ -174,7 +217,7 @@ def n_y_hit_dummy(question):
                     else:
                         print("you run away from the enemy")
 
-                    
+
         else:
             print("loading grassland")
             print("grassland loaded")
@@ -182,8 +225,8 @@ def n_y_hit_dummy(question):
 
             if answer7:
                 grasslandfile1()
-                    
-                
+
+
             else:
                 print("you____________enemy")
                 answer8 = query_yes_no("kill enemy?? ")
@@ -193,10 +236,10 @@ def n_y_hit_dummy(question):
 
                 else:
                     print("you run away from the enemy")
-                                
+
     else:
         print("you have not killed the dummy.")
-        
+
         answer6 = query_yes_no("take fabric??(from dummy) ")
 
         if answer6:
@@ -209,7 +252,7 @@ def n_y_hit_dummy(question):
                 print("loading grassland")
                 print("grassland loaded")
                 grasslandfile1()
-                
+
             else:
                 state["inventory"]["fabric"] += 6
                 print("inventory:")
@@ -217,12 +260,12 @@ def n_y_hit_dummy(question):
                 print("loading grassland")
                 print("grassland loaded")
                 answer12 = query_yes_no("harvest some grass?? ")
-                
+
 
                 if answer12:
                     grasslandfile1()
-                    
-                    
+
+
                 else:
                     print("you____________enemy")
                     answer13 = query_yes_no("kill enemy?? ")
@@ -232,19 +275,19 @@ def n_y_hit_dummy(question):
 
                     else:
                         print("you run away from the enemy")
- 
-                
-            
+
+
+
         else:
             print("the dummy is just standing there")
             print("loading grassland")
             print("grassland loaded")
             answer14 = query_yes_no("harvest some grass?? ")
-                
+
 
             if answer14:
-                grasslandfile1()                    
-                    
+                grasslandfile1()
+
             else:
                 print("you____________hammer")
                 answer15 = query_yes_no("get hammer?? ")
@@ -254,23 +297,23 @@ def n_y_hit_dummy(question):
                     print("you pick up the hammer.it is a x-speed hammer.you gain 1.3 x-speed.")
                     addspeed("xspeed", 1.3)
                     state['inventory']["x-speed"] += 1
-                    
+
                 else:
                     print("you go away from the hammer")
-                        
-            
+
+
 def start():
-    
+
     print("a")
     print("game")
     print("by")
     print("[40Bit]")
 
-    
+
     print("——————")
     print("QUESTX")
     print("——————")
-    
+
     answer = query_yes_no("melee??")
     if answer:
         print("melee » |")
@@ -283,7 +326,7 @@ def start():
             print("you->_sm_dummy")
 
         n_y_hit_dummy("hit dummy?")
-            
+
     else:
         print("ranged ˘ |")
         answer2 = query_yes_no("go to dark caverns?? ")
@@ -294,8 +337,8 @@ def start():
             print("loading sunshine rocks")
             print("you___________sr_dummy")
 
-        n_y_hit_dummy("shoot dummy?? ")          
-            
+        n_y_hit_dummy("shoot dummy?? ")
+
 
 
 start()
--- Loop[40Bit].py
+++ Loop[40Bit].py
@@ -1,4 +1,8 @@
 value = "40Bit"
 
 for x in range(0, 999999999999999999999):
+    value = "40Bit"
     print(value)
+    value = "_____"
+    print(value)
+    
