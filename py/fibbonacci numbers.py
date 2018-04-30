1d06ea335fe3d90b8e4f3998deb3b2ec2ee8746e
40Bit <ayugithub@amitk.me>
2018-04-30 09:39:26 -0700

    fibbonacci numbers program
    fibbonacci numbers program

---
+++ py/fibbonacci numbers.py
@@ -0,0 +1,36 @@
+import time
+
+a = 0
+b = 1
+for x in range(999):
+    c=a+b
+    time.sleep(.2)
+    print(c)
+    print('/o')
+    print('/^')
+    a=b
+    b=c
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
