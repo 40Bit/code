1cb7c14a0280ed76034fdb058c505a51c0994c6b
40Bit <ayugithub@amitk.me>
2018-11-16 09:58:16 -0800

    bash

---
+++ greekgodchooser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir ~/Desktop/~greek~god~chooser~
+mkdir ~/Desktop/~greek~god~chooser~/ Do_you_like_the_sky?
+mkdir ~/Desktop/~greek~god~chooser~/YES
+mkdir ~/Desktop/~greek~god~chooser~/NO
+
+mkdir ~/Desktop/~greek~god~chooser~/YES/ Do_you_like_bright_things?
+mkdir ~/Desktop/~greek~god~chooser~/YES/YES
+mkdir ~/Desktop/~greek~god~chooser~/YES/NO
+
+mkdir ~/Desktop/~greek~god~chooser~/YES/YES/ Would_you_be_rather_be_roasted_or_shocked?
+mkdir ~/Desktop/~greek~god~chooser~/YES/YES/SHOCKED
+mkdir ~/Desktop/~greek~god~chooser~/YES/YES/ROASTED
+
+mkdir ~/Desktop/~greek~god~chooser~/NO/ Do_you_like_swimming?
+mkdir ~/Desktop/~greek~god~chooser~/NO/YES
+mkdir ~/Desktop/~greek~god~chooser~/NO/NO
+
+mkdir ~/Desktop/~greek~god~chooser~/NO/NO/ Do_you_like_fire?
+mkdir ~/Desktop/~greek~god~chooser~/NO/NO/YES
+mkdir ~/Desktop/~greek~god~chooser~/NO/NO/NO
+
+mkdir ~/Desktop/~greek~god~chooser~/YES/NO/You_are_Uranus
+mkdir ~/Desktop/~greek~god~chooser~/NO/YES/You_are_Poseidon
+mkdir ~/Desktop/~greek~god~chooser~/NO/NO/YES/You_are_Hephaestus
+mkdir ~/Desktop/~greek~god~chooser~/NO/NO/NO/You_are_Hermes
+mkdir ~/Desktop/~greek~god~chooser~/YES/YES/SHOCKED/You_are_Zeus
+mkdir ~/Desktop/~greek~god~chooser~/YES/YES/ROASTED/You_are_Apollo
---
+++ gridmove.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yourname='hello'
+
+mkdir ~/Desktop/~gridmove~
+mkdir ~/Desktop/~gridmove~/--ROLE_SELECTION--
+mkdir ~/Desktop/~gridmove~/knight
+mkdir ~/Desktop/~gridmove~/archer
+mkdir ~/Desktop/~gridmove~/sorcerer
