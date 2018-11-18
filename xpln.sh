9e7dbe51653e8635d71713540c8857423184c775
40Bit <ayugithub@amitk.me>
2018-11-18 09:24:47 -0800

    Delete xpln.sh

--- xpln.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "THIS WAS WRITTEN IN BASH" > ~/Documents/code/html/xpln.html
-open -e ~/Documents/code/html/xpln.html
-sleep 5
-open -a Chromium https://apple.com https://google.com
