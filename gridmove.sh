5d8080437e741c418aff2df09409d5329f9cd0ab
40Bit <ayugithub@amitk.me>
2018-11-18 09:24:42 -0800

    Delete gridmove.sh

--- gridmove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yourname='hello'
-
-mkdir ~/Desktop/~gridmove~
-mkdir ~/Desktop/~gridmove~/--ROLE_SELECTION--
-mkdir ~/Desktop/~gridmove~/knight
-mkdir ~/Desktop/~gridmove~/archer
-mkdir ~/Desktop/~gridmove~/sorcerer
