57e402c30519e5e71a01caeec106bc34b021a3fc
40Bit <ayugithub@amitk.me>
2019-11-14 12:04:57 -0800

    Update valentineDaySequence.py

--- py/mainpy/valentineDaySequence.py
+++ py/mainpy/valentineDaySequence.py
@@ -2,22 +2,54 @@
 
 import time
 import math
-n = input('n: ')
+import turtle
+turtle.Screen().title('visualization')
+
+turtle.penup()
+turtle.right(90)
+turtle.forward(325)
+turtle.pendown()
+turtle.right(180)
+n = input('n: ') # 2048
 n = int(n)
+
 x = 0
+a = 0
+z = 0
+q = 3
+
 
 while True:
     if n % 2 != 0:
         n *= 3
         n += 1
+        turtle.color('red')
 
     else:
         n /= 2
+        turtle.color('blue')
 
-    time.sleep(.03)
+    time.sleep(.003)
     n = int(n)
     print(f'{x}. {n}')
+    
     x += 1
+    
+    if z == 0:
+        turtle.right(20)
+        turtle.forward(n / q)
+        a = 0
 
-    if n == 1:
+    if z == 1:
+        turtle.left(20)
+        turtle.forward(n / q)
+        a = 1
+
+    if n == 1 or n == -1:
         break
+
+    if a == 0:
+        z += 1
+
+    if a == 1:
+        z -= 1
