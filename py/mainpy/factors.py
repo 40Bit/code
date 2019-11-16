27299a75c8ed66c59ca66abfed5518f70c247ebb
40Bit <ayugithub@amitk.me>
2019-11-15 19:13:46 -0800

    commenting factors.py

--- py/mainpy/factors.py
+++ py/mainpy/factors.py
@@ -1,15 +1,15 @@
-n = 10 # 10: 1, 2, 5
-fList = []
-i = 0
+n = 10 # change for different numbers
+fList = [] # get fList to see all factors
+i = 0 # i is the value of all the factors combined
 y = 0
 
-def get_factors(n, fList):
+def get_factors(n, fList): # function for getting factors from number (n)
     for x in range(1, n):
         if n % x == 0:
             fList.append(x)
     return fList
 
-def adp(a, z):
+def adp(a, z): # function for ADP
     if a > z:
         return 'deficient'
 
